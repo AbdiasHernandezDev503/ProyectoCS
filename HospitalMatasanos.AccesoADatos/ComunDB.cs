61cf561f1a8c2363e6711b7f91a8eac00dba562e
Abdías Hernández <abdiashdz034@outlook.com>
2023-05-13 00:15:36 -0600

    Agregar las clases DAL

---
+++ HospitalMatasanos.AccesoADatos/ComunDB.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalMatasanos.AccesoADatos
+{
+    public class ComunDB
+    {
+        const string StrConexion = @"Data Source=LAPTOP-CN5T4MQA\SQLEXPRESS;Initial Catalog=BD_HOSPITAL_MATASANOS;Integrated Security=True";
+
+
+        private static SqlConnection ObtenerConexion()
+        {
+            SqlConnection connection = new SqlConnection(StrConexion);
+            connection.Open();
+            return connection;
+        }
+        public static SqlCommand ObtenerComando()
+        {
+            SqlCommand command = new SqlCommand();
+            command.Connection = ObtenerConexion();
+            return command;
+        }
+        public static int EjecutarComando(SqlCommand pComando)
+        {
+            int resultado = pComando.ExecuteNonQuery();
+            pComando.Connection.Close();
+            return resultado;
+        }
+        public static SqlDataReader EjecutarComandoReader(SqlCommand pComando)
+        {
+            SqlDataReader reader = pComando.ExecuteReader(CommandBehavior.CloseConnection);
+            return reader;
+        }
+    }
+}
--- HospitalMatasanos.AccesoADatos/TipoLicenciaDAL.cs
+++ HospitalMatasanos.AccesoADatos/TipoLicenciaDAL.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace HospitalMatasanos.AccesoADatos
 {
-    public class Class1
+    public class TipoLicenciaDAL
     {
     }
 }
